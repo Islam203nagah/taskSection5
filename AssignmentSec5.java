f06344869c8b21dca917d712498e353a580f51a7
Islam203nagah <92743825+Islam203nagah@users.noreply.github.com>
2021-11-20 18:36:33 +0200

    Update AssignmentSec5.java

--- AssignmentSec5.java
+++ AssignmentSec5.java
@@ -15,16 +15,26 @@ public class AssignmentSec5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       MemeberFactory memef = new MemeberFactory();
-       String MemeName="doctor";
-       String MemName2="teaching Assistant"; 
-       Staff stff = memef.getMember(MemeName);
-       stff.getSallary();
-       stff.getYearsOfWork();
-       
-       Staff stff2 = memef.getMember(MemName2);
-       stff2.getSallary();
-       stff2.getYearsOfWork();
+     //       MemeberFactory memef = new MemeberFactory();
+//       String MemeName="doctor";
+//       String MemName2="teaching Assistant"; 
+//       Staff stff = memef.getMember(MemeName);
+//       stff.getSallary();
+//       stff.getYearsOfWork();
+//       
+//       Staff stff2 = memef.getMember(MemName2);
+//       stff2.getSallary();
+//       stff2.getYearsOfWork();
+AbstractMember CSam= memberProducer.GetDepartment("CS") ;
+		Staff CSstfDR = CSam.GetMember("Doctor");
+		Staff CSstfTS = CSam.GetMember("teaching assistant");
+		CSstfDR.getSallary();
+		CSstfTS.getSallary();
+		AbstractMember ISam= MemberProducer.GetDepartment("is") ;
+		Staff ISstfDR = ISam.GetMember("Doctor");
+		Staff ISstfTS = ISam.GetMember("teaching assistant");
+		ISstfDR.getSallary();
+		ISstfTS.getSallary();
     }
     
 }
